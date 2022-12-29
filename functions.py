3389113222eb7a543129ab79aef1a799a8c5b810
CasualEd <94349386+CasualEd@users.noreply.github.com>
2022-12-29 13:12:42 +0100

    Added input validation, more comments and the ability to create stronger keys

--- functions.py
+++ functions.py
@@ -2,7 +2,8 @@
 import random as rd
 import hashlib 
 import math
-
+import numbthy as nb
+from os.path import exists
 #The goal of this function is to optimize the gcd function
 #Since the Euclidean algorithm is the fastest way to calculate the gcd, we will be using that algorithm in our function
 
@@ -82,7 +83,6 @@ def multiplicative_modular_inverse(a,b):
 #In order to check if a number is prime or not we check every number from 1 to it's square root can divide it or not
 #If that's not the case then it is prime
 def isprime(a):
-
     bool=True
     squ_a=int(math.sqrt(a))
     i=2
@@ -93,45 +93,49 @@ def isprime(a):
         i+=1
     return bool
 
-def generate_Keys(path_secret,path_pub):
-    easy=10**(-13)
-    print("Before finding a random p ")
-    p=rd.randint(10**14,10**17)                            #p=rd.randint((10**22)*easy,(10**27)*easy)  #We first generate p and q
-    q=rd.randint(10**14,10**17)
-    bool=False
-    print("Before looking at the prime level of p ")
-    while bool==False:
-        p+=1
-        bool=isprime(p)
-    bool=False
-    print("Before calculating q")
-    while bool==False:
-        q+=1
-        bool=isprime(q)
-    print("Before calculating n")
-    n=p*q                               #With p and q we get n
-    e=rd.randint(0,2*n)                #We then generate e
-    phi_n=(p-1)*(q-1)
-    bool=False
-    while bool==False:
-        e+=1
-        bool=gcd(e,phi_n)==1
-    print("Before calculating d")
-    d=multiplicative_modular_inverse(e,phi_n)
-    pub_file=open(path_pub,"w")
+
+def generate_Keys(path_secret,path_pub,strength):
+    strength=strength*20                                          #This is a variable that is used to determine the strengh of the keys
+    trusted_key=False                                             #This variable is an indicator on whether the key can be used to sign messages
+    while trusted_key==False:                                          
+        p=rd.randint(10**(strength-10),10**(strength))           #We first generate p and q
+        q=rd.randint(10**(strength-10),10**(strength))
+        bool=False
+        while bool==False:
+            p+=1
+            bool=nb.is_prime(p)
+        bool=False
+        while bool==False:
+            q+=1
+            bool=nb.is_prime(q)
+        n=p*q                               #With p and q we get n
+        e=rd.randint(0,2*n)                #We then generate e
+        phi_n=(p-1)*(q-1)
+        bool=False
+        while bool==False:
+            e+=1
+            bool=gcd(e,phi_n)==1
+        trusted_key=True
+        d=multiplicative_modular_inverse(e,phi_n)
+        pub_key_1=pubkey(n,e)
+        secret_key_1=secretkey(d,p,q)
+        randomstring=str(rd.randint(10**30,10**45))             #Here we test if the set of keys created actually work or not by generating a random string
+        signature=Sign(pub_key_1,secret_key_1,randomstring)     #We do this just because there is a slight degree of uncertainty concerning the answers given by the is_prime function
+        reconstructed_string=str(Verify_Signature(pub_key_1,signature))
+        if reconstructed_string==randomstring:
+            trusted_key=True
+    pub_file=open(path_pub,"w")                                 #We then proceed to write the public and secret key into two different files
     Pub_content=str(n)+'##'+str(e)
     pub_file.write(Pub_content)
     pub_file.close()
     secret_file=open(path_secret,'w')
     Secret_content=str(d)+'##'+str(p)+'##'+str(q)
     secret_file.write(Secret_content)
-    secret_file.close()
-    pub_key_1=pubkey(n,e)
-    secret_key_1=secretkey(d,p,q)
-    return [pub_key_1,secret_key_1]
+    secret_file.close() 
+    return [pub_key_1,secret_key_1]                             #And return the two keys 
 
-def Sign(pub_key,secret_key,signable,isFile=False):
-    if isFile!=True:
+def Sign(pub_key,secret_key,signable,isFile=False):                             #This functions is used to sign either a file or a string
+    if isFile!=True:                                                            #We first transform the object into a binary object whether it's a file or a string
         b_signable=signable.encode()
         hash=hashlib.sha256(b_signable)
     else:
@@ -140,23 +144,23 @@ def Sign(pub_key,secret_key,signable,isFile=False):
         b_content_file=signable_file.read()
         signable_file.close()
         hash=hashlib.sha256(b_content_file)
-    hash=hash.hexdigest()
+    hash=hash.hexdigest()                                                       #We then hash the binary object
     hash=int(hash,16)
     pr_hash=hash%pub_key.n
     pr_hash=hex(int(pr_hash))
-    signed=optimized_power(hash,secret_key.d,pub_key.n)  
-    signed=hex(int(signed))
+    signed=optimized_power(hash,secret_key.d,pub_key.n)                        #And put it to the power of d with modulo n to obtain the signature
+    signed=hex(int(signed))                                                    #We then put it into an hexadecimal form
     return signed
 
-def Verify_Signature(public_key,signed_object,hash_object=None,object=None,isFile=False):
-    signed_object=int(signed_object,16)
-    hash=optimized_power(signed_object,public_key.e,public_key.n)
+def Verify_Signature(public_key,signed_object,hash_object=None,object=None,isFile=False):    #This function is used to verify a signature of a string or a file using the public key
+    signed_object=int(signed_object,16)                             #Transforming the signature that is an hexadecimal value into an int
+    hash=optimized_power(signed_object,public_key.e,public_key.n)   #We put it to the power of e with modulo n to recover the original hash
     hash=hex(int(hash))
-    if hash_object is None and object is None:
+    if hash_object is None and object is None:                      #If the user only has the signature we can only send back the hash
         return hash
-    elif object is None:
+    elif object is None:                                            #If the user has the hash of the object, we can verify that it does correspond to the hash that was calculated
         return hash==hash_object
-    elif hash_object is None and isFile==False:
+    elif hash_object is None and isFile==False:                     #Otherwise we calculate the hash of the given object and compare it to the hash we obtained in the first part of the function
         b_object=object.encode()
         hash_object=hashlib.sha256(b_object)
     elif hash_object is None and isFile:
@@ -172,7 +176,7 @@ def Verify_Signature(public_key,signed_object,hash_object=None,object=None,isFil
     return hash==hash_object
 
 
-def import_Keys(path,type):
+def import_Keys(path,type):                             #This function is used to import keys from txt files using the predefined formats "d##p##q" for secret key files and "n##e" for public key files         
     if type=='secret':
         secret_file=open(path,'r')
         secret_contents=secret_file.read().split('##')
@@ -189,3 +193,39 @@ def import_Keys(path,type):
         public_key=pubkey(n,e)
         return public_key
         
+
+def correct_input(msg,type,minim=-10**6,maxim=10**6):                #This function is used to do some input validation. msg is a variable that corresponds to the message printed on the screen for the input function                     
+    if type==1:                                                      #type corresponds to the type of variable that we want either an input, a path or a string 
+        correct_inputv=False
+        loop_done_once=False
+        user_input=minim-1
+        while correct_inputv==False or (user_input<minim or user_input>maxim):
+            if loop_done_once:
+                print("Error: Incorrect input")
+            try:
+                user_input=int(input(msg))
+                correct_inputv=True
+            except:
+                correct_inputv=False     
+            loop_done_once=True
+    elif type==2:
+        path_exists=False
+        loop_done_once=False
+        while path_exists==False:
+            if loop_done_once:
+                print("Error: Incorrect input")
+            user_input=input(msg)
+            path_exists=exists(user_input)
+            loop_done_once=True    
+    else:
+        user_input=None
+        loop_done_once=False
+        while user_input is None:
+            if loop_done_once:
+                print("Error: Incorrect input")
+            user_input=input(msg)
+            loop_done_once=True
+    return user_input
+
+
+    
--- main.py
+++ main.py
@@ -9,42 +9,43 @@ def main():
     print("                                                                          RSA: Signature\n")
     print("This is a program designed to sign strings and files using the RSA algorithm\n")
     while(answer!=5):
-        print("What do you want to do ?")
+        print("\nWhat do you want to do ?")
         print("Answer 1 if you want to create a set of keys")
         print("Answer 2 if you want to import a set of keys from files")
         print("Answer 3 if you want to sign a string or a file")
         print("Answer 4 if you want to verify a signature for a file or a string")
         print("Answer 5 if you want to exit the program")
-        S=input(">>>> ")
-        correct_input=False
-        while correct_input==False:
-            try: 
-                answer=int(S)
-                correct_input=True
-            except:
-                S=input("Incorrect input "+ending)
+        answer=functions.correct_input(ending,1,1,5)
         if answer==1:
             filename_pub=''
             incorrect_path=True
             while filename_pub=='' and incorrect_path:
-                filename_pub=input("What shall be the path to the file containing the public key : ")
+                filename_pub=input("What shall be the path to the file containing the public key : "+ending)
                 incorrect_path=exists(filename_pub)
             filename_secret=''
             incorrect_path=True
             while filename_secret=='':
-                filename_secret=input("What shall be the path to the file containing the secret key : ")
+                filename_secret=input("What shall be the path to the file containing the secret key : "+ending)
                 incorrect_path=exists(filename_pub)
-            [Pub_Key,Secret_Key]=functions.generate_Keys(filename_secret,filename_pub)
+            strength=11
+            while strength<1 or strength>10:
+                try:
+                    strength=int(input("What shall be the strength of your keys ? Pick a number between 1 and 10 "+ending))
+                except:
+                    strength=12
+                    print("Error: Wrong type of answer ")
+            print("Generating keys....")
+            [Pub_Key,Secret_Key]=functions.generate_Keys(filename_secret,filename_pub,strength)
             has_public_key=True
             has_secret_key=True
 
             print("The public and secret key have been generated and can now be used")
         elif answer==2:
-            Files_chosen=int(input('Answer 1 if you just want to import a public key \nAnswer 2 if you want to import both keys '+ending))
+            Files_chosen=functions.correct_input('Answer 1 if you just want to import a public key \nAnswer 2 if you want to import both keys '+ending,1,1,2)
             if Files_chosen==2:
                 filename_secret=''
                 incorrect_path=True
-                while filename_secret=='':
+                while filename_secret=='' and incorrect_path:
                     filename_secret=input("What is the path of the file containing the secret key : ")
                     incorrect_path=exists(filename_secret)
                 Secret_Key=functions.import_Keys(filename_secret,'secret')
@@ -58,31 +59,33 @@ def main():
             has_public_key=True
         elif answer==3:
             if has_public_key and has_secret_key:
-                isFile=int(input('Answer 1 if you want to sign a file \nAnswer 2 if you want to sign a string '+ending))
+                isFile=functions.correct_input('Answer 1 if you want to sign a file \nAnswer 2 if you want to sign a string '+ending,1,1,2)
                 if isFile==1:
-                    filename=input('What is the path of the file ? '+ending)
+                    filename=functions.correct_input('What is the path of the file ? '+ending,2)
                     print("The signature of the file is :"+str(functions.Sign(Pub_Key,Secret_Key,filename,True)))
                 else:
-                    object=input('What is the string you want to sign ?'+ending)
+                    object=functions.correct_input('What is the string you want to sign ?'+ending,3)
                     print("The object is : "+object)
                     print("The signature of the string is : "+str(functions.Sign(Pub_Key,Secret_Key,object)))
             else:
                 print("You don't have a secret key or a public key. Please generate one or import one before you can sign.")
         elif answer==4:
             if has_public_key:
-                Signature=input("What is the signature ?"+ending)
-                has_object=int(input("Do you have the file or the string ?\nAnswer 1 if you do >>>> "))
+                Signature=None
+                while Signature is None:
+                    Signature=input("What is the signature ?"+ending)
+                has_object=functions.correct_input("Do you have the file or the string ?\nAnswer 1 if you do"+ending,1)
                 if has_object==1:
-                    isFile=int(input('Answer 1 if you want to verify the signature of a file \nAnswer 2 if you want to verify the signature of a string '+ending))
+                    isFile=functions.correct_input('Answer 1 if you want to verify the signature of a file \nAnswer 2 if you want to verify the signature of a string '+ending,1,1,2)
                     if isFile==1:
-                        filename=input('What is the path of the file ? '+ending)
+                        filename=functions.correct_input('What is the path of the file ? '+ending,2)
                         Correct_Signature=functions.Verify_Signature(Pub_Key,Signature,None,filename,isFile=True)
                         if Correct_Signature:
                             print("The signature does correspond to the file")
                         else: 
                             print("The signature does not correspond to the file")
                     else:
-                        object=input('What is the string ?'+ending)
+                        object=functions.correct_input('What is the string ?'+ending,3)
                         print("The object is :"+object)
                         Correct_Signature=functions.Verify_Signature(Pub_Key,Signature,None,object,isFile=False)
                         if Correct_Signature:
